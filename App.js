029b4fb3a07e0998cf36e3b49d24312954f228f5
MUNIF <munif@shellbeehaken.com>
2024-02-20 09:31:50 +0600

    push notification implementation done

--- App.js
+++ App.js
@@ -1,7 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Linking, StyleSheet, Text, View } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { useEffect } from "react";
 
 export default function App() {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  };
+  const getInitialURL = async () => {
+    const url = await Linking.getInitialURL();
+    if (typeof url === "string") {
+      return url;
+    }
+    //getInitialNotification: When the application is opened from a quit state.
+    await messaging().getInitialNotification();
+  };
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      //return fcm token for the device
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    } else {
+      console.log("failed token status", authStatus);
+    }
+
+    //getInitialNotification: When the application is opened from a quit state.
+    getInitialURL();
+
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log(remoteMessage.notification);
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +65,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
